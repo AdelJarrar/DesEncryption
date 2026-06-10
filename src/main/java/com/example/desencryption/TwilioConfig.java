9a6b9e648871c94a132cdceb7853cc1fa4e54412
adelj <adeljarrar97@gmail.com>
2026-06-10 14:10:38 +0300

    add to the project

--- src/main/java/com/example/desencryption/TwilioConfig.java
+++ src/main/java/com/example/desencryption/TwilioConfig.java
@@ -4,7 +4,7 @@ public final class TwilioConfig {
 
     public static final String ACCOUNT_SID = "AC6b907ccce9db080acf3602ac68c2c0fb";
 
-    public static final String AUTH_TOKEN = "3a8a97aa634b0d10026037bcb1cd7565";
+    public static final String AUTH_TOKEN = "c0333dcab0369072851930b122b6ac3a";
 
     public static final String FROM_PHONE = "+15722204455";
 
